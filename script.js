03654ce6551372112fac6aa9043ccef5302435f8
priyanshu-debug26 <priyanshu7pandey@gmail.com>
2025-11-28 10:58:15 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const quotes = [
+    "📻 BOX BOX....", "📻 NOOOOOOOO", "📻 Must be The Water", "📻 Leave me Alone", "📻 Yabadabadi", "📻 Pole Position"
+];
+
+const quoteElement = document.querySelector(".quote");
+
+function randomQuotes(){
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    quoteElement.textContent = randomQuote;
+}
+
+randomQuotes();
+
+setInterval(randomQuotes, 3000);
+
+const images = [
+    "Sources/h1.jpg", "Sources/h2.webp", "Sources/h3.webp", "Sources/h4.jpg", "Sources/h5.jpg", "Sources/h6.jpg", "Sources/h7.jpg"
+];
+
+let heroIndex = 0;
+
+const imgElement = document.getElementById("f1img");
+
+function randomImage(){
+    imgElement.style.opacity = 0;
+
+    setTimeout(() => {
+        heroIndex = (heroIndex + 1) % images.length;
+        imgElement.src = images[heroIndex];
+        imgElement.style.opacity = 1;
+    }, 200)
+}
+
+setInterval(randomImage, 5000);
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".loader").classList.add("fade");
+    }, 4000)
+})
